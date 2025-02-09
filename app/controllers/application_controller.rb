ac1703f291f09833eddf9eb7d5c14fa8bbd041dd
aaonykj21 <aon.natcha29@gmail.com>
2025-02-10 00:23:49 +0700

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  helper_method :current_user
+  include Authentication
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  def current_user
+    @current_user ||=User.find(session[:user_id]) if session[:user_id]
+    @current_user 
+  end
+end
